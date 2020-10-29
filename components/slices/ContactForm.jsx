49b50dcadf76d58a9c4ff63b1caed26371491e8d
Gianmarco Ciaffardini <marco@chelsea-apps.com>
2020-10-29 11:31:31 +0100

    add mail endpoint

--- components/slices/ContactForm.jsx
+++ components/slices/ContactForm.jsx
@@ -1,7 +1,10 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import axios from 'axios'
 
 const ContactForm = () => {
+
+    const [isSending, setIsSending] = useState(false)
+    const [resMessage, setResMessage] = useState("")
 
     const nameRef = useRef("")
     const emailRef = useRef("")
@@ -9,12 +12,22 @@ const ContactForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setIsSending(true)
+
         const { value: name } = nameRef.current
         const { value: email } = emailRef.current
         const { value: message } = messageRef.current
 
-        const res = await axios.post('/api/mail', { name, email, message })
-        console.log(res)
+        const { data } = await axios.post('/api/mail', { name, email, message })
+        setIsSending(false)
+        
+        setResMessage(data.message)
+
+        if(data.success){
+            nameRef.current.value = ""
+            emailRef.current.value = ""
+            messageRef.current.value = ""
+        }
     }
 
     return (
@@ -23,7 +36,15 @@ const ContactForm = () => {
                 <input type="text" ref={nameRef} placeholder="Nome" required className="w-full mb-3 h-10 pl-2"/>
                 <input type="email" ref={emailRef} placeholder="Email" required className="w-full mb-3 h-10 pl-2"/>
                 <textarea ref={messageRef} placeholder="Messaggio" required className="w-full mb-3 h-40 pl-2"/>
-                <button className="bg-secondary font-bold uppercase text-white px-10 py-2 rounded-full mt-3">Invia</button>
+                {resMessage &&
+                    <p>{resMessage}</p>
+                }
+                <button 
+                    className="bg-secondary font-bold uppercase text-white px-10 py-2 rounded-full mt-3" 
+                    disabled={isSending}
+                >
+                        {isSending ? 'Inviando..' : 'Invia'}
+                </button>
             </form>
         </div>
     )
--- pages/api/mail.js
+++ pages/api/mail.js
@@ -4,20 +4,23 @@ const sgMail = require('@sendgrid/mail');
 export default (req, res) => {
     if (req.method === 'POST') {
         const { name, email, message } = req.body
-        console.log(name, email, message)
-        res.send('success')
+
+        res.json({
+            success: true,
+            message: 'Messaggio inviato, le risponderemo il prima possibile.'
+        })
         // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
         // const msg = {
         //     to: 'support@quantum.gg',
         //     from: 'support@quantum.gg',
-        //     subject: 'Contact Request - Quantum Website',
-        //     html: `<p>Name: ${name}</p> <p>Email: ${email}</p> <p>Company: ${company}</p> <p>Phone: ${phone}</p> <p>Message: ${message}</p>`,
+        //     subject: 'Richiesta di contatto - Lumafarmbio Website',
+        //     html: `<p>Nome: ${name}</p> <p>Email: ${email}</p> <p>Messaggio: ${message}</p>`,
         // };
         // sgMail.send(msg)
-        // .then(response => res.status(200).json({ success: true, msg:'Message Sent! We will get back in touch asap!', response }))
+        // .then(response => res.status(200).json({ success: true, message:'Messaggio inviato, le risponderemo il prima possibile.' }))
         // .catch(err => {
         //     console.log(err)
-        //     res.status(500).json({ success: false, msg: 'Oops! Something went wrong', err })
+        //     res.status(500).json({ success: false, message: 'Oops! Qualcosa è andato storto.. })
         // })
       } else {
           res.send('Post only')
