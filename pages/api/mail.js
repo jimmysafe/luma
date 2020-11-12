dbe2a61457a792bb9165a9c75c84a1998e5c0b1b
Gianmarco Ciaffardini <marco@chelsea-apps.com>
2020-11-12 16:49:36 +0100

    add sendgrid key and mail endpoint

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -9,21 +9,20 @@ export default (req, res) => {
             success: true,
             message: 'Messaggio inviato, le risponderemo il prima possibile.'
         })
-        // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-        // const msg = {
-        //     to: 'support@quantum.gg',
-        //     from: 'support@quantum.gg',
-        //     subject: 'Richiesta di contatto - Lumafarmbio Website',
-        //     html: `<p>Nome: ${name}</p> <p>Email: ${email}</p> <p>Messaggio: ${message}</p>`,
-        // };
-        // sgMail.send(msg)
-        // .then(response => res.status(200).json({ success: true, message:'Messaggio inviato, le risponderemo il prima possibile.' }))
-        // .catch(err => {
-        //     console.log(err)
-        //     res.status(500).json({ success: false, message: 'Oops! Qualcosa è andato storto.. })
-        // })
-      } else {
-          res.send('Post only')
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+        const msg = {
+            to: 'info@lumafarmbio.it',
+            from: 'info@lumafarmbio.it',
+            subject: 'Richiesta di contatto - Lumafarmbio Website',
+            html: `<p>Nome: ${name}</p> <p>Email: ${email}</p> <p>Messaggio: ${message}</p>`,
+        };
+        sgMail.send(msg)
+        .then(response => 
+            res.status(200).json({ success: true, message:'Messaggio inviato, le risponderemo il prima possibile.' })
+        )
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ success: false, message: 'Oops! Qualcosa è andato storto..' })
+        })
       }
-  }
-  
+}
