5a31d16f66065be5c8d9e7493638a0f28419b6d4
Gianmarco Ciaffardini <marco@chelsea-apps.com>
2020-11-14 12:35:38 +0100

    add proper api endpoint

--- components/common/FooterCard.jsx
+++ components/common/FooterCard.jsx
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 
 const FooterCard = () => {
     return (
         <div className="p-6 rounded text-white text-center bg-secondary max-w-sm flex justify-center items-center flex-col">
             <h4 className="uppercase font-bold text-xl">contatti</h4>
             <p className="my-3 text-sm px-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, corporis?</p>
-            <div className="rounded-full bg-primary px-4 py-2 text-gray-600 font-bold uppercase">
-                contattaci
-            </div>
+            <Link as="/contatti" href="/[page]">
+                <div className="rounded-full bg-primary px-4 py-2 text-gray-600 font-bold uppercase cursor-pointer">
+                    contattaci
+                </div>
+            </Link>
         </div>
     )
 }
